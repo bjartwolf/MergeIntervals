a3971295ff25a5b78711e817c720d1441dea37be
Bjørn Einar <bjartnes@gmail.com>
2017-07-02 08:31:48 +0200

    Ingen er enige

--- MergeIntervals/Tests.fs
+++ MergeIntervals/Tests.fs
@@ -29,4 +29,12 @@ let ``Readable and Bjorn Einar agree`` (baseList: (int*int) list) =
                        |> List.map (fun (x,y) -> if x < y then (x,y) else (y,x))
     Readable.fmo testList = BjornEinar.fmo testList 
 
+[<Property(Verbose = true)>]
+let ``Readable and Einar agree`` (baseList: (int*int) list) = 
+    let testList = baseList
+                       |> List.map (fun (x,y) -> (abs x, abs y)) 
+                       |> List.map (fun (x,y) -> if x = y then (x,y+1) else (x,y))
+                       |> List.map (fun (x,y) -> if x < y then (x,y) else (y,x))
+    Readable.fmo testList = Einar.fmo testList 
+
 
