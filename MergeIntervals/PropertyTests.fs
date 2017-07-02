ddf6b58615a46dc94d811b3e91cd931c7f047513
Bjørn Einar <bjartnes@gmail.com>
2017-07-02 12:19:05 +0200

    simplify

--- MergeIntervals/PropertyTests.fs
+++ MergeIntervals/PropertyTests.fs
@@ -2,24 +2,18 @@
 open FsCheck 
 open FsCheck.Xunit
 
-let rnd = System.Random()
-
 [<Property(Verbose = true)>]
 let ``ImmutableStack and mutableStack agree`` (baseList: (int*int) list) = 
     let testList = baseList
                        |> List.map (fun (x,y) -> (abs x, abs y)) 
-                       |> List.map (fun (x,y) -> if x = y then (x,y+1) else (x,y))
                        |> List.map (fun (x,y) -> if x < y then (x,y) else (y,x))
-                       |> List.distinct
     ImmutableStack.fmo testList = MutableStack.fmo testList 
 
 [<Property(Verbose = true)>]
 let ``Original and MutableStack agree`` (baseList: (int*int) list) = 
     let testList = baseList
                        |> List.map (fun (x,y) -> (abs x, abs y)) 
-                       |> List.map (fun (x,y) -> if x = y then (x,y+1) else (x,y))
                        |> List.map (fun (x,y) -> if x < y then (x,y) else (y,x))
-                       |> List.distinct
     Original.fmo testList = MutableStack.fmo testList 
 
 
@@ -27,18 +21,14 @@ let ``Original and MutableStack agree`` (baseList: (int*int) list) =
 let ``Readable and ImmutableStack agree`` (baseList: (int*int) list) = 
     let testList = baseList
                        |> List.map (fun (x,y) -> (abs x, abs y)) 
-                       |> List.map (fun (x,y) -> if x = y then (x,y+1) else (x,y))
                        |> List.map (fun (x,y) -> if x < y then (x,y) else (y,x))
-                       |> List.distinct
     Readable.fmo testList = ImmutableStack.fmo testList 
 
 [<Property(Verbose = true)>]
 let ``Readable and mutableStack agree`` (baseList: (int*int) list) = 
     let testList = baseList
                        |> List.map (fun (x,y) -> (abs x, abs y)) 
-                       |> List.map (fun (x,y) -> if x = y then (x,y+1) else (x,y))
                        |> List.map (fun (x,y) -> if x < y then (x,y) else (y,x))
-                       |> List.distinct
     Readable.fmo testList = MutableStack.fmo testList 
 
 
