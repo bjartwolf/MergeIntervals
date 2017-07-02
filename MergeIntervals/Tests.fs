b808461f5afb9b472f0f4c6c0817c65bb11940ec
Bjørn Einar <bjartnes@gmail.com>
2017-07-02 11:54:35 +0200

    Guess 19 is right at open intervals

--- MergeIntervals/Tests.fs
+++ MergeIntervals/Tests.fs
@@ -1,16 +1,19 @@
 ﻿module Tests
 open Xunit
 
-let  testdata = [(3,4);(6,7)]
-
+let testdata_sum_4 = [(3,4);(6,7)]
+let testdata_sum_19  = [0, 8; 10,19; 11,19]
 [<Fact>]
 let ``Original handles a simple case``() =
-    Assert.Equal (Original.fmo testdata, 4)
+    Assert.Equal(Original.fmo testdata_sum_4, 4)
+    Assert.Equal(Original.fmo testdata_sum_19, 19)
 
 [<Fact>]
 let ``Bjorn Einar handles a simple case``() =
-    BjornEinar.fmo testdata = 4
+    Assert.Equal(BjornEinar.fmo testdata_sum_4, 4)
+    Assert.Equal(BjornEinar.fmo testdata_sum_19, 19)
 
 [<Fact>]
 let ``Einar handles a simple case``() =
-    Einar.fmo testdata = 4
+    Assert.Equal(Einar.fmo testdata_sum_4, 4)
+    Assert.Equal(Einar.fmo testdata_sum_19, 19)
