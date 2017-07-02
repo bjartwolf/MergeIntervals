3eaabaf6a64ed1ce459dbed3c66a43824af42a1f
Bjørn Einar <bjartnes@gmail.com>
2017-07-02 12:16:40 +0200

    more tests

--- MergeIntervals/PropertyTests.fs
+++ MergeIntervals/PropertyTests.fs
@@ -14,7 +14,7 @@ let ``ImmutableStack and mutableStack agree`` (baseList: (int*int) list) =
     ImmutableStack.fmo testList = MutableStack.fmo testList 
 
 [<Property(Verbose = true)>]
-let ``Original and MmutableStack agree`` (baseList: (int*int) list) = 
+let ``Original and MutableStack agree`` (baseList: (int*int) list) = 
     let testList = baseList
                        |> List.map (fun (x,y) -> (abs x, abs y)) 
                        |> List.map (fun (x,y) -> if x = y then (x,y+1) else (x,y))
--- MergeIntervals/Tests.fs
+++ MergeIntervals/Tests.fs
@@ -6,23 +6,35 @@ let testdata_singleInterval = [(1,1)]
 
 let testdata_sum_4 = [(3,4);(6,7)]
 let testdata_sum_19  = [0, 8; 10,19; 11,19]
-let testdata_from_fscheck_sum_4 = [(3, 3); (0, 3); (0, 0)]
+let testdata_from_fscheck_sum_5 = [(3, 4); (0, 3); (0, 1)]
+
+[<Fact>]
+let ``Readable handles weird fscheckdata``() =
+    Assert.Equal(5, Readable.fmo testdata_from_fscheck_sum_5)
 
 [<Fact>]
 let ``MutableStack handles weird fscheckdata``() =
-    Assert.Equal(MutableStack.fmo testdata_from_fscheck_sum_4, 4)
+    Assert.Equal(5, MutableStack.fmo testdata_from_fscheck_sum_5)
 
 [<Fact>]
 let ``Original handles weird fscheckdata``() =
-    Assert.Equal(Original.fmo testdata_from_fscheck_sum_4, 4)
+    Assert.Equal(5, Original.fmo testdata_from_fscheck_sum_5)
+
+[<Fact>]
+let ``Readable handles empty interval ``() =
+    Assert.Equal(0, Readable.fmo testdata_empty)
+
+[<Fact>]
+let ``Readable handles single interval ``() =
+    Assert.Equal(1, Readable.fmo testdata_singleInterval)
+
+[<Fact>]
+let ``Original handles single interval ``() =
+    Assert.Equal(1, Original.fmo testdata_singleInterval)
 
 [<Fact>]
 let ``MutableStack handles single interval ``() =
     Assert.Equal(MutableStack.fmo testdata_singleInterval, 1)
-
-[<Fact>]
-let ``Original handles single interval ``() =
-    Assert.Equal(Original.fmo testdata_singleInterval, 1)
 
 [<Fact>]
 let ``ImmutableStack handles single interval ``() =
